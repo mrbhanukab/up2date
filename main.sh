79fab85d306506abdd9af8b5657f3428a5886d64
mrbhanukab <mrbhanuka@outlook.com>
2021-10-25 13:13:14 +0530

    added command

--- main.sh
+++ main.sh
@@ -12,22 +12,28 @@ read -p 'What is Your Linux Distro? [1/2/3]:' os
 
 if [ $os == 1 ]
 then
-	sudo apt update -y
-	sudo apt upgrade -y
-	sudo apt-get install -y ubuntu-restricted-extras
-	sudo apt install -y gnome-tweaks
-	sudo apt install -y neofetch
-	sudo apt install -y flatpak
-	sudo apt install -y sl
-	sudo apt-get install -y tlp
-	sudo tlp start
-	sudo apt install -y htop
-	sudo apt install -y vim
-	sudo apt-add-repository ppa:fish-shell/release-3
-	sudo apt install fish
-	chsh -s /usr/bin/fish
-	git clone https://github.com/AdnanHodzic/auto-cpufreq.git
-    cd auto-cpufreq && sudo ./auto-cpufreq-installer
+    echo -e "\n"
+	tput bold 1; echo "The 'newos' Programme 👾" | sed  -e :a -e "s/^.\{1,$(tput cols)\}$/ & /;ta" | tr -d '\n' | head -c $(tput cols)
+	echo -e "\n"
+	echo -e "\033[1;31mPlease wait a few minutes, the time depends on your internet connection & pc performance. \033[1;0m⌛ \n"
+	echo -e "\033[1;36m ✅ Operating System : 'Ubuntu'"
+	echo -e "\033[1;36m 😎 User : $(whoami) \n"
+    echo -n -e "\033[1;32m 💻 Updating ..."; sudo apt update -y > /dev/null; echo -e " /n"; 
+	echo -n -e "\033[1;32m 💻 Upgradng ..."; sudo apt upgrade -y > /dev/null; echo -e " /n"; 
+	echo -n -e "\033[1;32m ⬇️ Installing Ubuntu Restricted Extras ..."; sudo apt-get install -y ubuntu-restricted-extras > /dev/null; echo -e " /n"; 
+	echo -n "\033[1;32m ⬇️ Installing Gnome Tweaks ..."; sudo apt install -y gnome-tweaks > /dev/null; echo -e " /n"; 
+	echo -n -e "\033[1;32m ⬇️ Installing neofetch ..."; sudo apt install -y neofetch > /dev/null; echo -e " /n"; 
+	echo -n -e "\033[1;32m ⬇️ Installing flatpak ..."; sudo apt install -y flatpak > /dev/null; echo -e " /n"; 
+	echo -n -e "\033[1;32m ⬇️ Installing sl ..."; sudo apt install -y sl > /dev/null; echo -e " /n"; 
+	echo -n -e "\033[1;32m ⬇️ Installing tlp ..."; sudo apt-get install -y tlp > /dev/null; echo -e " /n"; 
+	echo -n -e "\033[1;34m ✅ Starting tlp ..."; sudo tlp start > /dev/null; echo -e " /n"; 
+	echo -n -e "\033[1;32m ⬇️ Installing htop ..."; sudo apt install -y htop > /dev/null; echo -e " /n"; 
+	echo -n -e "\033[1;32m ⬇️ Installing vim ..."; sudo apt install -y vim > /dev/null; echo -e " /n"; 
+	echo -n -e "\033[1;34m 💻 Adding repository ppa:fish-shell/release-3 ..."; sudo apt-add-repository ppa:fish-shell/release-3 > /dev/null; echo -e " /n"; 
+	echo -n -e "\033[1;32m ⬇️ Installing fish..."; sudo apt install fish > /dev/null; echo -e " /n"; 
+	echo -n -e "\033[1;34m 💻 Making fish is default ..."; chsh -s /usr/bin/fish > /dev/null; echo -e " /n"; 
+	echo -n -e "\033[1;34m 💻 Cloning auto-cpufreq.git ..."; git clone https://github.com/AdnanHodzic/auto-cpufreq.git > /dev/null; echo -e " /n"; 
+    echo -n -e "\033[1;32m ⬇️ Installing auto-cpufreq ..."; cd auto-cpufreq && sudo ./auto-cpufreq-installer > /dev/null; echo -e " /n"; 
 elif [ $os == 2 ]
 then
 	echo "Fedora"
