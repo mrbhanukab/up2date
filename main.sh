b480d83a9b49ff26cefbfe25ca2e677c093b58b4
mrbhanukab <mrbhanuka@outlook.com>
2021-10-25 12:24:06 +0530

    add commands

--- main.sh
+++ main.sh
@@ -12,24 +12,22 @@ read -p 'What is Your Linux Distro? [1/2/3]:' os
 
 if [ $os == 1 ]
 then
-	echo -e "\n"
-	tput bold 1; echo "The 'newos' Programme 👾" | sed  -e :a -e "s/^.\{1,$(tput cols)\}$/ & /;ta" | tr -d '\n' | head -c $(tput cols)
-	echo -e "\n"
-	echo -e "\033[1;36m ✅ Operating System : 'Ubuntu'"
-	echo -e "\033[1;36m 😎 User : $(whoami) \n"
-	echo -e "\n"
-       	echo -e "\033[31;1m$user, First we need to update the our system ⬇️  \033[31;0m"
-	echo -e "\n"
-	echo -e "\033[1;34mSo, I am Running 'sudo apt update' command \033[1;0m \n"
-	echo -e "\033[1;31mPlease wait a few minutes, the time depends on your internet connection & pc performance. \033[1;0m⌛ \n\n"
 	sudo apt update -y
-        'clear'
-        echo -e "\n"
-        tput bold 1; echo "The 'newos' Programme 👾" | sed  -e :a -e "s/^.\{1,$(tput cols)\}$/ & /;ta" | tr -d '\n' | head -c $(tput cols)
-	echo -e "\n"
-	echo -e "\033[1;34m Now, I am Running 'sudo apt upgrade' command \033[1;0m"
-        echo -e "\n \033[1;31mPlease wait a few minutes, the time depends on your internet connection & pc performance. \033[1;0m⌛ \n\n"
 	sudo apt upgrade -y
+	sudo apt-get install -y ubuntu-restricted-extras
+	sudo apt install -y gnome-tweaks
+	sudo apt install -y neofetch
+	sudo apt install -y flatpak
+	sudo apt install -y sl
+	sudo apt-get install -y tlp
+	sudo tlp start
+	sudo apt install -y htop
+	sudo apt install -y vim
+	sudo apt-add-repository ppa:fish-shell/release-3
+	sudo apt install fish
+	chsh -s /usr/bin/fish
+	git clone https://github.com/AdnanHodzic/auto-cpufreq.git
+    cd auto-cpufreq && sudo ./auto-cpufreq-installer
 elif [ $os == 2 ]
 then
 	echo "Fedora"
